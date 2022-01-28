2bfc2a8997dadd91c488cd5826abf959b919b28b
gksang <sangkipgil@gmail.com>
2022-01-28 20:33:43 +0300

    Dev icon Image Click Jquery update

--- js/jquery.js
+++ js/jquery.js
@@ -1,3 +1,6 @@
+// ---- Jquery Code to Hover and Add Image Description-----
+
+
 $(document).ready(function() {
 
     $('.overlay').hover(
@@ -11,4 +14,19 @@ $(document).ready(function() {
        }
     );
         
+ });
+
+//  -------Jquery Code on Image Click on What We Do section------
+$('#des-icon').on('click', function(e)  {
+    $('.click-desc-des').fadeIn()
+    $('#des-icon').css({"display": "none"});
+    $('#des-icon-title').css({"display": "none"});
+     
+ });
+ 
+ $('.click-desc-des').on('click', function(e)  {   
+    $('.click-desc-des').css({"display": "none"});
+    $('#des-icon').fadeIn()
+    $('#des-icon-title').fadeIn()
+     
  });
