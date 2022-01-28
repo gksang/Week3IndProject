c98386b87d192caceb2709120f24a299d0aee332
gksang <sangkipgil@gmail.com>
2022-01-28 17:58:20 +0300

    CONTACT section Html content and form add

--- js/jquery.js
+++ js/jquery.js
@@ -1,1 +1,14 @@
-// alert("Im working");
+$(document).ready(function() {
+
+    $('.overlay').hover(
+        
+       function () {
+          $(this).css({"opacity":"5"});
+       }, 
+        
+       function () {
+          $(this).css({"opacity":"0"});
+       }
+    );
+        
+ });
